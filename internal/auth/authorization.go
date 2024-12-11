bf6d616f149930913b25d733640de5ca723c8fc3
Stefan Netterfield <stefan@5f03.com>
2024-12-11 02:51:57 -0500

    Wrote logic for fetching permissions, roles, and checking if the use has roles with the required permissions needed to be authorized

---
+++ internal/auth/authorization.go
@@ -0,0 +1,171 @@
+package auth
+
+import (
+	"github.com/VEDA95/OpenBoard-API/internal/db"
+	"github.com/doug-martin/goqu/v9"
+	"slices"
+)
+
+type Role struct {
+	Id          string       `json:"id" db:"id,omitempty"`
+	Name        string       `json:"name" db:"name,omitempty"`
+	Permissions []Permission `json:"permissions" db:"-"`
+}
+
+type Permission struct {
+	Id     string `json:"id" db:"id,omitempty"`
+	Path   string `json:"path" db:"path,omitempty"`
+	RoleId string `json:"-" db:"role_id,omitempty"`
+}
+
+func IsUserAuthorized(userId string, permissions ...string) (bool, error) {
+	if len(permissions) == 0 {
+		return true, nil
+	}
+
+	roles := make([]string, 0)
+	err := db.Instance.From("open_board_user_roles").Prepared(true).
+		Select("role_id").
+		Where(goqu.Ex{"user_id": userId}).
+		ScanVals(&roles)
+
+	if err != nil {
+		return false, err
+	}
+
+	if len(roles) == 0 && len(permissions) > 0 {
+		return false, nil
+	}
+
+	rolePermissions := make([]string, 0)
+	err2 := db.Instance.From("open_board_role_permissions").Prepared(true).
+		Select("permission.path").
+		Where(goqu.Ex{"role_id": roles}).
+		Join(goqu.T("open_board_role_permission"), goqu.On(goqu.Ex{
+			"open_board_role_permissions.permission_id": "open_board_role_permission.id",
+		})).
+		As("permission").
+		ScanVals(&rolePermissions)
+
+	if err2 != nil {
+		return false, err2
+	}
+
+	if len(rolePermissions) == 0 && len(permissions) > 0 {
+		return false, nil
+	}
+
+	for _, permission := range permissions {
+		if !slices.Contains(rolePermissions, permission) {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func GetRoles() ([]Role, error) {
+	roles := make([]Role, 0)
+	err := db.Instance.From("open_board_roles").Prepared(true).
+		Select("*").
+		ScanStructs(&roles)
+
+	if err != nil {
+		return nil, err
+	}
+
+	roleIds := make([]string, len(roles))
+
+	for _, role := range roles {
+		roleIds = append(roleIds, role.Id)
+	}
+
+	permissions := make([]Permission, 0)
+	err2 := db.Instance.From("open_board_role_permissions").Prepared(true).
+		Select("role_id", goqu.C("permission.id").As("id"), goqu.C("permission.path").As("path")).
+		Where(goqu.Ex{"role_id": roleIds}).
+		Join(goqu.T("open_board_role_permission"), goqu.On(goqu.Ex{
+			"open_board_role_permissions.permission_id": "open_board_role_permission.id",
+		})).
+		As("permission").
+		ScanStructs(&permissions)
+
+	if err2 != nil {
+		return nil, err2
+	}
+
+	for _, permission := range permissions {
+		for index := range roles {
+			if roles[index].Id == permission.RoleId {
+				roles[index].Permissions = append(roles[index].Permissions, permission)
+				break
+			}
+		}
+	}
+
+	return roles, nil
+}
+
+func GetRole(roleId string) (*Role, error) {
+	var role Role
+	exists, err := db.Instance.From("open_board_roles").Prepared(true).
+		Select("*").
+		Where(goqu.Ex{"id": roleId}).
+		ScanStruct(&role)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !exists {
+		return nil, nil
+	}
+
+	permissions := make([]Permission, 0)
+	err2 := db.Instance.From("open_board_role_permissions").Prepared(true).
+		Select(goqu.C("permission.id").As("id"), goqu.C("permission.path").As("path")).
+		Where(goqu.Ex{"role_id": roleId}).
+		Join(goqu.T("open_board_role_permission"), goqu.On(goqu.Ex{
+			"open_board_role_permissions.permission_id": "open_board_role_permission.id",
+		})).
+		As("permission").
+		ScanStructs(&permissions)
+
+	if err2 != nil {
+		return nil, err2
+	}
+
+	for _, permission := range permissions {
+		role.Permissions = append(role.Permissions, permission)
+	}
+
+	return &role, nil
+}
+
+func GetBoardPermissions(boardId string) ([]Permission, error) {
+	permissions := make([]Permission, 0)
+	err := db.Instance.From("open_board_board_permissions").Prepared(true).
+		Select(goqu.C("permission.id").As("id"), goqu.C("permission.path").As("path")).
+		Where(goqu.Ex{"board_id": boardId}).
+		ScanStructs(&permissions)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
+
+func GetWorkspacePermissions(workspaceId string) ([]Permission, error) {
+	permissions := make([]Permission, 0)
+	err := db.Instance.From("open_board_workspace_permissions").Prepared(true).
+		Select(goqu.C("permission.id").As("id"), goqu.C("permission.path").As("path")).
+		Where(goqu.Ex{"workspace_id": workspaceId}).
+		ScanStructs(&permissions)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
